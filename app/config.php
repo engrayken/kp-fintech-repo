c0025c97d52680f56ab46ad149da56febd5a5844
Ayogu Kenneth <48656985+kenneth2718@users.noreply.github.com>
2023-03-05 05:21:21 +0100

    Update config.php

--- app/config.php
+++ app/config.php
@@ -4,12 +4,12 @@
 
 //change the values to your database settings
 define('DB_HOST', 'localhost'); //localhost
-define('DB_USER', 'kenspayc_p');  //username
-define('DB_PASSWORD', 'C95FiXf[-95ojN');  //password
-define('DB_NAME', 'kenspayc_pay'); //database name
-define('URL', 'https://kenspay.com.ng'); //end url with '/'
+define('DB_USER', 'root');  //username
+define('DB_PASSWORD', 'password');  //password
+define('DB_NAME', 'dbname'); //database name
+define('URL', 'https://example.com'); //end url with '/'
 $url=URL; 
-$kenhost=('kenspay.com.ng'); //end url with '/'
+$kenhost=('example.com'); //end url with '/'
 
 if (version_compare(phpversion(), '5.4.0', '<') == true) {
 	exit('PHP5.4+ Required');
